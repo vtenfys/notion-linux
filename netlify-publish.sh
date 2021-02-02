0a3a310df80a64e0ed539d09245037244149c196
David Bailey <davidbailey00@outlook.com>
2021-02-02 18:22:20 +0000

    fix wget url in netlify script

--- netlify-publish.sh
+++ netlify-publish.sh
@@ -3,5 +3,5 @@ set -e
 
 mkdir -p public
 cd public
-wget https://api.cirrus-ci.com/v1/artifact/github/davidbailey00/notion-linux-builder/Create RPM repo/rpm_repo.zip
+wget 'https://api.cirrus-ci.com/v1/artifact/github/davidbailey00/notion-linux-builder/Create RPM repo/rpm_repo.zip'
 unzip rpm_repo.zip
