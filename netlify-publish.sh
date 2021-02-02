caf3ed67b82cd2a35f3f4cb7cc7d8fe18eeb6af1
David Bailey <davidbailey00@outlook.com>
2021-02-02 18:20:51 +0000

    setup netlify ci

---
+++ netlify-publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+mkdir -p public
+cd public
+wget https://api.cirrus-ci.com/v1/artifact/github/davidbailey00/notion-linux-builder/Create RPM repo/rpm_repo.zip
+unzip rpm_repo.zip
