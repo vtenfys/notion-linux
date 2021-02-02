5ad07bd345934cb5e8e094a67776190ae4f5324b
David Bailey <davidbailey00@outlook.com>
2021-02-02 18:56:22 +0000

    rewrite netlify deploy task

--- scripts/netlify-publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p public
-cd public
-wget -q 'https://api.cirrus-ci.com/v1/artifact/github/davidbailey00/notion-linux-builder/Create RPM repo/rpm_repo.zip'
-unzip rpm_repo.zip
